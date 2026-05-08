d2b15e211f0f84edc70c2168f897e701de90d021
dio <er.dioneda@gmail.com>
2026-05-08 13:49:46 -0400

    feat: Added niri ipc stream for reading focused window event

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,51 @@
 use std::fs::{self, File};
-use std::io::{Result, prelude::*};
+use std::io::{BufReader, BufWriter, Result, prelude::*};
 use std::path::Path;
 use std::thread;
 use std::time::Duration;
+use std::os::unix::net::UnixStream;
 
 use tracing::{info, Level};
 use tracing_subscriber::FmtSubscriber;
 
-fn main() -> Result<()>{
-     let path = Path::new("/tmp/sasha/foo.txt");
+use dotenvy::dotenv;
+use std::env;
 
+fn main() -> Result<()>{
      let subscriber = FmtSubscriber::builder()
          .with_max_level(Level::TRACE)
          .finish();
-     
-     if let Some(parent) = path.parent() {
-         fs::create_dir_all(parent)?;
-     }
      tracing::subscriber::set_global_default(subscriber)
          .expect("Failed to set tracing subscriber!");
 
+     info!("Starting sasha daemon...");
+     dotenv().expect("Failed to load .env file");
+     info!("Successfully loaded environment variables...");
+
+     let niri_socket_path = env::var("NIRI_SOCKET_PATH").expect("Set your niri's socket path.");
+
+     let path = Path::new("/tmp/sasha/foo.txt");
+     let writer_stream = UnixStream::connect(niri_socket_path)?;
+     let reader_stream = writer_stream.try_clone()?;
+
+     let mut reader = BufReader::new(reader_stream);
+     let mut writer = BufWriter::new(writer_stream);
+
+     if let Some(parent) = path.parent() {
+         fs::create_dir_all(parent)?;
+     }
 
     let mut file = File::create("/tmp/sasha/foo.txt")?;
     loop {
-        file.write_all(b"Hello. I'm Sasha, a Rust daemon\n")?;
+        writer.write_all(b"\"FocusedWindow\"\n")?;
+        writer.flush()?;
+
+        let mut response = String::new();
+        reader.read_line(&mut response)?;
+
+        writeln!(file, "{response}")?;
         file.flush()?;
-        info!("Sasha heartbeat...");
+
         thread::sleep(Duration::from_secs(1));
     }
 }
